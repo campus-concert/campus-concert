59c82ce085c24f6bae13c8d4f874a5bc31d056dc
Einar Midthun <89466996+einar-m@users.noreply.github.com>
2023-12-05 14:15:09 -1000

    WIP Profile filters

--- app/imports/ui/pages/BrowseProfiles.jsx
+++ app/imports/ui/pages/BrowseProfiles.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Col, Container, Row, Card } from 'react-bootstrap';
+import { Col, Container, Row, Card, Dropdown } from 'react-bootstrap';
 import { useTracker } from 'meteor/react-meteor-data';
 import { Profiles } from '../../api/profile/Profile';
 import LoadingSpinner from '../components/LoadingSpinner';
@@ -8,15 +8,58 @@ import ProfileBasic from '../components/ProfileBasic';
 
 const BrowseProfiles = () => {
 
+  const [locationFilter, setLocationFilter] = useState('All locations');
+  const [instrumentFilter, setInstrumentFilter] = useState('All instruments');
+  const [tasteFilter, setTasteFilter] = useState('All tastes');
+  const [filteredProfiles, setFilteredProfiles] = useState();
+
   const { ready, profiles } = useTracker(() => {
     const subscription = Meteor.subscribe(Profiles.userPublicationName);
     const rdy = subscription.ready();
     const profileItems = Profiles.collection.find({}).fetch();
+    setFilteredProfiles(profileItems);
     return {
       profiles: profileItems,
       ready: rdy,
     };
   }, []);
+
+  const filterLocation = () => {
+    console.log('Filtering by location: ', locationFilter);
+    if (locationFilter === 'All locations') {
+      console.log('Outside if statement');
+      return profiles;
+    }
+    console.log('Inside if statement');
+    return profiles.filter((profile) => (profile.location === locationFilter));
+  };
+
+  const filterInstruments = () => {
+    if (instrumentFilter !== 'All instruments') {
+      return profiles.filter((profile) => (profile.instruments.includes(instrumentFilter)));
+    }
+    return profiles;
+  };
+
+  const filterTastes = () => {
+    if (tasteFilter !== 'All tastes') {
+      return profiles.filter((profile) => (profile.tastes.includes(tasteFilter)));
+    }
+    return profiles;
+  };
+
+  const filterProfiles = () => {
+    let filtering = filterLocation();
+    filtering = filterInstruments();
+    filtering = filterTastes();
+    setFilteredProfiles(filtering);
+    console.log('Done filtering. Profiles are now: ', filtering);
+  };
+
+  const updateLocationFilter = (location) => {
+    setLocationFilter(location);
+    filterProfiles();
+  };
 
   return ready ? (
     <Container id="browse-profiles-page" className="py-3">
@@ -26,8 +69,32 @@ const BrowseProfiles = () => {
             <Col className="text-center">
               <h2>Browse Profiles</h2>
             </Col>
+            <Row>
+              <Col>
+                <h4>Filter by Location:</h4>
+                <Dropdown>
+                  <Dropdown.Toggle variant="primary" id="dropdown-location">
+                    {locationFilter}
+                  </Dropdown.Toggle>
+                  <Dropdown.Menu>
+                    <Dropdown.Item onClick={() => updateLocationFilter('All locations')}>
+                      All locations
+                    </Dropdown.Item>
+                    <Dropdown.Item onClick={() => updateLocationFilter('Honolulu')}>
+                      Honolulu
+                    </Dropdown.Item>
+                    <Dropdown.Item onClick={() => updateLocationFilter('Pearl City')}>
+                      Pearl City
+                    </Dropdown.Item>
+                    <Dropdown.Item onClick={() => updateLocationFilter('Kailua')}>
+                      Kailua
+                    </Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+              </Col>
+            </Row>
             <Row xs={1} md={2} lg={3} className="g-4">
-              {profiles.map((profile, index) => (
+              {filteredProfiles.map((profile, index) => (
                 <Col key={index}>
                   <ProfileBasic profile={profile} />
                 </Col>
